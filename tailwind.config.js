570780e6f7f6e8e56fe0501789ccc0a24cedef3a
Dheeraj <dkjoshi8293@gmail.com>
2024-02-11 13:44:34 +0530

    Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  important: true,
+  // important: true,
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
