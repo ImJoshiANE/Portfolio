777e5645dd5bf407936c278e22ab87a4e493330f
Dheeraj Kumar <dheerajkumar@Dheerajs-MacBook-Air.local>
2024-12-30 16:23:55 +0530

    Added microsoft clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Footer from "@/Components/Footer";
 import Navbar from "@/Components/Navbar";
+import Head from 'next/head';
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -16,6 +17,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+          {/* Microsoft Clarity Script */}
+          <script
+              type="text/javascript"
+              dangerouslySetInnerHTML={{
+                  __html: `
+                      (function(c,l,a,r,i,t,y){
+                          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                      })(window, document, "clarity", "script", "pm0kpb2i4p");
+                  `,
+              }}
+          />
+      </Head>
       <body
         className={`${montserrat.variable} font-sans bg-light dark:bg-dark w-full min-h-screen`}
       >
